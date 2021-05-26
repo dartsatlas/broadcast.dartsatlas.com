9cbee1e87f3330e7b28be32c4cb3926a6b2854be
Matt Langan <mattlangan@users.noreply.github.com>
2021-05-26 10:48:23 -0500

    Add text color to theme support

--- obs_assets/darts_atlas_match.lua
+++ obs_assets/darts_atlas_match.lua
@@ -1,7 +1,9 @@
 obs = obslua
 match_hash_id = "#########"
 da_theme_color = "00AE3E"
+da_text_color = "FFFFFF"
 base_url = "https://www.dartsatlas.com/matches/"
+-- base_url = "https://dartsatlas.ngrok.io/matches/"
 
 function activate(activating)
   print('activate')
@@ -55,15 +57,15 @@ function reset(pressed)
   local match_standings = obs.obs_get_source_by_name("[DA] Standings")
 
   local match_best_of_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_best_of")
-  local match_bracket_url = (tostring(base_url) .. tostring(match_hash_id) .. "/bracket")
+  local match_bracket_url = (tostring(base_url) .. tostring(match_hash_id) .. "/bracket?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
 --  local match_calendar_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_calendar")
-  local match_dart_counts_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_dart_counts?theme_color=" .. tostring(da_theme_color))
+  local match_dart_counts_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_dart_counts?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
   local match_location_p1_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_location?player=1")
   local match_location_p2_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_location?player=2")
-  local match_preview_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_preview")
-  local match_stats_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_stats")
-  local match_stats_stacked_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_tv_stats")
-  local match_summary_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_summary")
+  local match_preview_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_preview?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
+  local match_stats_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_stats?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
+  local match_stats_stacked_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_tv_stats?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
+  local match_summary_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_summary?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
   local match_name_p1_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_name?player=1")
   local match_name_p2_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_name?player=2")
 --[[
@@ -71,9 +73,9 @@ function reset(pressed)
   local match_photo_p2_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_photo?player=2")
 --]]
 --   local match_schedule_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_schedule")
-  local match_scoreboard_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_dual_cam?theme_color=" .. tostring(da_theme_color))
-  local match_scoreboard_stacked_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast?mode=tv&theme_color=" .. tostring(da_theme_color))
-  local match_standings_url = (tostring(base_url) .. tostring(match_hash_id) .. "/season_standings")
+  local match_scoreboard_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast_dual_cam?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
+  local match_scoreboard_stacked_url = (tostring(base_url) .. tostring(match_hash_id) .. "/broadcast?mode=tv&theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
+  local match_standings_url = (tostring(base_url) .. tostring(match_hash_id) .. "/season_standings?theme_color=" .. tostring(da_theme_color) .. "&text_color=" .. tostring(da_text_color))
 
   local settings = obs.obs_data_create()
 
@@ -172,6 +174,7 @@ function reset_button_clicked(props, p)
   activate(false)
   match_hash_id = obs.obs_data_get_string(settings, "match_hash_id")
   da_theme_color = obs.obs_data_get_string(settings, "da_theme_color")
+  da_text_color = obs.obs_data_get_string(settings, "da_text_color")
   reset(true)
 end
 
@@ -179,6 +182,7 @@ function script_properties()
   local props = obs.obs_properties_create()
   obs.obs_properties_add_text(props, "match_hash_id", "Match Hash ID", obs.OBS_TEXT_DEFAULT)
   obs.obs_properties_add_text(props, "da_theme_color", "Theme Color", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "da_text_color", "Text Color", obs.OBS_TEXT_DEFAULT)
   obs.obs_properties_add_button(props, "reset_button", "Reset Match", reset_button_clicked)
   return props
 end
@@ -191,5 +195,6 @@ function script_update(settings)
   activate(false)
   match_hash_id = obs.obs_data_get_string(settings, "match_hash_id")
   da_theme_color = obs.obs_data_get_string(settings, "da_theme_color")
+  da_text_color = obs.obs_data_get_string(settings, "da_text_color")
   reset(true)
 end
